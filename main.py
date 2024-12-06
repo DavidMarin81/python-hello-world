218ea1b716edab621d1613869a381476af7df9ae
David Marin <davidmarin81@gmail.com>
2024-12-06 23:37:01 +0100

    Second commit

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ from datetime import datetime
 
 print("Hola mundo desde Python")
 print(f"Fecha actual: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+print("Nueva linea para comprobar Github Actions")
